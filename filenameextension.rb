de032d72bcd63a4b6842ad549cedfd76561f65fe
21wh5a1211 <109098011+21wh5a1211@users.noreply.github.com>
2023-03-20 11:25:10 +0530

    Update filenameextension.rb

--- filenameextension.rb
+++ filenameextension.rb
@@ -1,14 +1,13 @@
 # file name
-fbname = File.basename "C:\home/it/hello.rb"
+fbname = File.basename ""C:\home\it/21wh5a1211.rb",".rb"
 puts "File name: "+fbname 
 
 # basename
-bname = File.basename "C:\home\it/hello shivani.rb",".rb"
+bname = File.basename "C:\home\it/21wh5a1211.rb",".rb"
 puts "Base name: "+bname
 
 # file extention
-ffextn = File.extname  "C:\home\it/hello shivani.rb",".rb"
-puts "Extention: "+ffextn 
+ffextn = File.extname "C:\home\it/21wh5a1211.rb",".rb" 
 # path name
 path_name= File.dirname  "C:\home\it/21wh5a1211.rb",".rb"
 puts "Path name: "+path_name

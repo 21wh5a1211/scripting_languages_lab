8c4eb7bf194eedff671e6bdacd9d2a65713ff64d
21wh5a1211 <109098011+21wh5a1211@users.noreply.github.com>
2023-03-20 10:55:58 +0530

    Update makes.rb

--- makes.rb
+++ makes.rb
@@ -1,9 +1,9 @@
 def makes20(x,y)
-    return x == 20 || y == 20 || x + y == 20
+    return x == 30 || y == 30 || x + y == 30
 end
 
-print makes20(20, 15),"\n"
+print makes20(30, 25),"\n"
 print makes20(30, 20),"\n"
-print makes20(45, 10),"\n"
-print makes20(20, 20),"\n"
+print makes20(55, 20),"\n"
+print makes20(30, 30),"\n"
 print makes20(20, 10),"\n"

4568e0beb6ca72b29942009bba04a0732de4717f
21wh5a1211 <109098011+21wh5a1211@users.noreply.github.com>
2023-03-20 11:02:38 +0530

    Update totalmarks.rb

--- totalmarks.rb
+++ totalmarks.rb
@@ -1,8 +1,8 @@
 
 student_marks = Hash.new 0
-student_marks['SL'] = 92
+student_marks['SL'] = 85
 student_marks['ES'] = 85
-student_marks['IoT'] = 95
+student_marks['IoT'] = 84
 total_marks = 0
 student_marks.each {|key,value|
               total_marks +=value
